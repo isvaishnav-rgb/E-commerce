4f6fadaa51b9295cc9ffa3280b91a3cc175b5b18
Isha Vaishnav <isvaishnav@bestpeers.in>
2026-01-20 18:47:38 +0530

    add changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,12 +16,12 @@ DBConnnect().then(() => {
 });
 
 const app = express();
-app.use(cors({
-    origin: true,
+app.use(
+  cors({
+    origin: "https://e-commerce-6-su5k.onrender.com/",
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+  })
+);
 
 app.use(express.json());
 
